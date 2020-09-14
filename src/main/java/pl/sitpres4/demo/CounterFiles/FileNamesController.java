028a72e74d3234481119108a3117bbce6fc1e66b
Marcin Kowalczyk <kovalmar@poczta.onet.pl>
2020-09-14 02:15:14 +0200

    /lastUpdated added for /files and /counters

--- src/main/java/pl/sitpres4/demo/CounterFiles/FileNamesController.java
+++ src/main/java/pl/sitpres4/demo/CounterFiles/FileNamesController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.sitpres4.demo.Data.DataFromFTP;
+
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -16,5 +18,10 @@ public class FileNamesController {
     @GetMapping("/files/refresh")
     public List<String> fileNamesRefresh() {
         return DataFromFTP.getInstance().getFileNames(true);
+    }
+
+    @GetMapping("/files/lastupdated")
+    public Date fileNamesLastUpdated() {
+        return DataFromFTP.getInstance().getLastUpdated(DataFromFTP.FILE_NAMES);
     }
 }
--- src/main/java/pl/sitpres4/demo/Counters/CountersController.java
+++ src/main/java/pl/sitpres4/demo/Counters/CountersController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.sitpres4.demo.Data.DataFromFTP;
 import pl.sitpres4.demo.DemoApplication;
 
+import java.util.Date;
+
 @RestController
 public class CountersController {
     @GetMapping("/counters")
@@ -15,5 +17,10 @@ public class CountersController {
     @GetMapping("/counters/refresh")
     public Counters countersRefresh() {
         return new Counters(true);
+    }
+
+    @GetMapping("/counters/lastupdated")
+    public Date countersLastUpdated() {
+        return DataFromFTP.getInstance().getLastUpdated(DataFromFTP.COUNTERS_FROM_CONFIG);
     }
 }
